f1f4ecf769c9af5505b5f986459dba38dafa98bb
ShaolunWang <wang.shaolun@gmail.com>
2026-03-19 20:52:36 +0800

    update readme
    misc: rename hello to main, remove program.scala

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # 3bit Emulator
 
+Multi-cycle 3bit program executor, with IF, EX, and WB stages
+- [x] end-to-end runnable
+- [x] unit tests
+- [x] pretty printer
+- [ ] parse from cli
 
 Pretty printer inspired by: https://gist.github.com/Mark1626/1221c1750e38d55fe605225cf877356c
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,7 @@ import machine.step3
 import machine.fetch
 import machine.execute
 
-@main def hello(): Unit =
+@main def main(): Unit =
   val prog = Vector(0, 1, 5, 4, 3, 0)
   var m = Machine(
     regs = Registers(x = 3729, y = 0, z = 0, out = Vector()),
@@ -15,6 +15,8 @@ import machine.execute
     ip = 0
   )
 
+  // NOTE: not using the runner because
+  // I'd like to pretty print for debugging here
   while (m.ip < prog.length) {
     m = step3(m)
     PPrint.pretty(m)
